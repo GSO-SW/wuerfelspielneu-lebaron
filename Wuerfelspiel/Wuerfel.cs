ca8d26b92c7d74365877b00782a520144d6532cb
Arian <ariansaati@gmail.com>
2022-04-01 18:48:41 +0200

    TestsErstellt(Constructor Used/Unused, Sicherung)

--- Wuerfelspiel/Wuerfel.cs
+++ Wuerfelspiel/Wuerfel.cs
@@ -4,5 +4,70 @@ namespace Wuerfelspiel
 {
     public class Wuerfel
     {
+        private int anzahlSeiten = 6;
+        private int letztesErgebniss;
+        private bool gesichert = false;
+        Random rnd;
+
+        public Wuerfel(int AnzahlSeiten)
+        {
+            this.anzahlSeiten = AnzahlSeiten;
+        }
+
+        public Wuerfel()
+        {
+
+        }
+
+        public int Wurfeln()
+        {
+            letztesErgebniss = rnd.Next(0, AnzahlSeiten + 1); ;
+            return rnd.Next(0, AnzahlSeiten + 1);
+        }
+
+        public void SicherungUmschalten()
+        {
+            if (gesichert == false)
+            {
+                gesichert = true;
+            }
+            if (gesichert == true)
+            {
+                gesichert = false;
+            }
+            else
+            {
+                gesichert = false;
+            }
+        }
+
+        public int AnzahlSeiten
+        {
+            get
+            {
+                return anzahlSeiten;
+            }
+        }
+
+        public int LetztesErgebniss
+        {
+            get
+            {
+                return rnd.Next(0, AnzahlSeiten + 1);
+            }
+        }
+
+        public bool Gesichert
+        {
+            get
+            {
+                return gesichert;
+            }
+
+            set
+            {
+                Gesichert = gesichert;
+            }
+        }
     }
 }
--- WuerfelspielTests/WuerfelTests.cs
+++ WuerfelspielTests/WuerfelTests.cs
@@ -6,13 +6,67 @@ namespace WuerfelspielTests
 {
     [TestClass]
     public class WuerfelTests
-    {
+    {  
         [TestMethod]
-        public void DummyTest()
+        public void Wurfel_Erstellt()
         {
-            // Dieser Test ist hier, um eine GitHub-Funktion zu testen. 
-            // Einfach löschen :)
-            Assert.IsTrue(true);// is tatsächlich true :O
+            //act
+            Wuerfel x = new Wuerfel();
+        } 
+
+        [TestMethod]
+        public void Wurfel_EmptyConstructor()
+        {
+            //arrange          
+            Wuerfel x = new Wuerfel();
+
+            //act
+            Wuerfel y = new Wuerfel();
+
+            //assert
+            Assert.AreEqual(y.AnzahlSeiten,x.AnzahlSeiten);
         }
+        
+        [TestMethod]
+        public void Wurfel_ConstructorIsUsed()
+        {
+            //arrange          
+            Wuerfel x = new Wuerfel(8);
+
+            //act
+            Wuerfel y = new Wuerfel(10);
+
+            //assert
+            Assert.AreEqual(8,x.AnzahlSeiten);
+            Assert.AreEqual(10,y.AnzahlSeiten);
+        }
+        
+        [TestMethod]
+        public void Wurfel_GesichertAufTrue()
+        {
+            //arrange    
+            bool gesichert = false;
+            Wuerfel x = new Wuerfel();
+
+            //act
+            x.SicherungUmschalten();
+
+            //assert
+            Assert.IsTrue(gesichert = true);
+        }
+        
+        [TestMethod]
+        public void Wurfel_GesichertAufFalse()
+        {
+            //arrange    
+            bool gesichert = true;
+            Wuerfel x = new Wuerfel();
+
+            //act
+            x.SicherungUmschalten();
+
+            //assert
+            Assert.AreNotEqual(gesichert, x.Gesichert);
+        }      
     }
 }
